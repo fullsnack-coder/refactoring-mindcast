71b26bfdceee0bb40a74ebb93cb261941e27014c
Manuel Garcia <33798495+fullsnack-coder@users.noreply.github.com>
2021-11-01 20:54:30 -0600

    chore: add clean scaffolding (#2)

---
+++ src/presentation/screens/index.ts
@@ -0,0 +1,3 @@
+const name = 'manuel';
+
+export {name};
---
+++ src/system/atoms/Button/Button.tsx
@@ -0,0 +1,7 @@
+import {Button as RNButton, ButtonProps as RNButtonProps} from 'react-native';
+
+type Props = {} & RNButtonProps;
+
+const Button: React.FC<Props> = props => <RNButton {...props} />;
+
+export default Button;
