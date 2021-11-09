ea0c45a0b1c588cff72ae09172b70c161935c99a
Manuel Garcia <manueldev250799@gmail.com>
2021-11-08 23:49:13 -0500

    feat: add large size to icon

--- src/system/atoms/Icon/Icon.tsx
+++ src/system/atoms/Icon/Icon.tsx
@@ -3,11 +3,13 @@ import VectorIcon from 'react-native-vector-icons/MaterialCommunityIcons'
 type VectorIconProps = React.ComponentProps<typeof VectorIcon>
 
 export type Props = {
-  size?: 'sm' | 'md'
+  size?: 'sm' | 'md' | 'lg'
 } & Omit<VectorIconProps, 'size'>
 
 const Icon: React.FC<Props> = ({ size = 'sm', ...rest }) => {
-  return <VectorIcon size={size === 'sm' ? 14 : 22} {...rest} />
+  return (
+    <VectorIcon size={size === 'sm' ? 14 : size === 'lg' ? 38 : 22} {...rest} />
+  )
 }
 
 export default Icon
