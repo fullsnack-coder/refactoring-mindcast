5ab4c12f142dc00554feb79157f6b4524f0c7a5c
Manuel Garcia <33798495+fullsnack-coder@users.noreply.github.com>
2021-11-17 00:03:13 -0500

    feat: add onboarding screen (#20)

--- src/presentation/screens/Onboarding/Onboarding.tsx
+++ src/presentation/screens/Onboarding/Onboarding.tsx
@@ -1,11 +1,21 @@
+import { useAppTheme } from '@application/hooks'
+import { AppStackParamList } from '@application/navigation/AppNavigator'
+import OnboardingSlider from '@presentation/containers/OnboardingSlider'
 import { useNavigation } from '@react-navigation/native'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 
-import OnboardingSlider from '@system/organisms/OnboardingSlider'
+import { StatusBar } from 'react-native'
+
+type Navigation = NativeStackNavigationProp<AppStackParamList, 'onboarding'>
 
 const OnboardingScreen: React.FC = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation<Navigation>()
+  const { colors } = useAppTheme()
   return (
-    <OnboardingSlider onComplete={() => navigation.navigate('auth' as never)} />
+    <>
+      <StatusBar backgroundColor={colors.primaryBackground} translucent />
+      <OnboardingSlider onComplete={() => navigation.navigate('auth')} />
+    </>
   )
 }
 
--- src/presentation/screens/YourInterests/YourInterests.tsx
+++ src/presentation/screens/YourInterests/YourInterests.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from 'react-native'
+import { useAppTheme } from '@application/hooks'
+import Box from '@system/atoms/Box'
+import Typography from '@system/atoms/Typography'
 
-const YourInterestsScreen: React.FC = () => (
-  <View>
-    <Text>hello from your interests screen</Text>
-  </View>
-)
+import { StatusBar } from 'react-native'
+
+const { Text } = Typography
+
+const YourInterestsScreen: React.FC = () => {
+  const { colors } = useAppTheme()
+  return (
+    <Box
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
+      bg="primaryBackground">
+      <StatusBar translucent backgroundColor={colors.primaryBackground} />
+      <Text>Hello from your interests screen</Text>
+    </Box>
+  )
+}
 
 export default YourInterestsScreen
