a574343b4ca2b993d7d9c6cfd675c4337ed2feef
Manuel Garcia <manueldev250799@gmail.com>
2021-11-23 22:52:06 -0500

    feat: authors & podcasts stores added

--- src/application/store/rootReducer.ts
+++ src/application/store/rootReducer.ts
@@ -1,11 +1,15 @@
 import { combineReducers } from 'redux'
 
 import authReducer from './modules/auth'
+import authorsReducer from './modules/authors'
+import podcastsReducer from './modules/podcasts'
 import topicsReducer from './modules/topics'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   topics: topicsReducer,
+  podcasts: podcastsReducer,
+  authors: authorsReducer,
 })
 
 export default rootReducer
