2e1c94d4282bdc0e2371028ee3c8b966db316be1
Manuel Garcia <manueldev250799@gmail.com>
2021-11-24 22:51:32 -0500

    chore: change application orientation to portrait locked

--- App.tsx
+++ App.tsx
@@ -4,11 +4,17 @@ import AppNavigator from '@application/navigation'
 import store from '@application/store'
 import { ThemeProvider } from '@shopify/restyle'
 import appTheme from '@system/theme'
+import Orientation from 'react-native-orientation-locker'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { Provider } from 'react-redux'
+import { useEffect } from 'react'
 
 const App: React.FC = () => {
+  useEffect(() => {
+    Orientation.lockToPortrait()
+  }, [])
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={appTheme}>
