870df380f7c407fe5733e17227eecdc832f05a05
Manuel Garcia <manueldev250799@gmail.com>
2021-11-24 22:51:10 -0500

    chore: add react-native-orientation-locker

--- android/app/src/main/java/com/mindcasttwo/MainActivity.java
+++ android/app/src/main/java/com/mindcasttwo/MainActivity.java
@@ -1,9 +1,19 @@
 package com.mindcasttwo;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.content.res.Configuration;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is
