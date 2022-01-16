da1d0315e2093546c051a980c0f657567555ad15
Manuel Garcia <manueldev250799@gmail.com>
2022-01-15 19:47:22 -0500

    fix: set isLoading to false when remove process ends

--- src/application/store/modules/downloads/episodes.ts
+++ src/application/store/modules/downloads/episodes.ts
@@ -259,6 +259,7 @@ const downloadEpisodesReducer = (
         return {
           ...state,
           episodes: state.episodes.filter(({ id }) => id !== episodeId),
+          isLoading: false,
         }
       }
       return state
