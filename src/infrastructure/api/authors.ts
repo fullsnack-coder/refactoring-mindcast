3247f8eea39013000554623868f81b6bd6ea9742
Manuel Garcia <33798495+fullsnack-coder@users.noreply.github.com>
2022-01-19 23:08:01 -0500

    feat: add search authors and podcast by topics services (#61)

--- src/infrastructure/api/authors.ts
+++ src/infrastructure/api/authors.ts
@@ -1,6 +1,7 @@
 //TODO: change this mock value by podcast real backend
 
 import { Author, Podcast } from '@application/types'
+import { sleep } from '@application/utils/tools'
 import { trendingAuthors, podcasts } from '@infrastructure/mock/apiData'
 
 export const getAuthorInfo = async (authorId: string) => {
@@ -11,32 +12,45 @@ export const getAuthorInfo = async (authorId: string) => {
   return authorInfo
 }
 
+export type GetAuthorsOptions = {
+  name?: string
+  topicTag?: string
+  order?: 'asc' | 'desc'
+  size?: number
+}
+
 export type TrendingOptions = {
   size?: number
   order?: 'asc' | 'desc'
+  name?: string
+  topicTag?: string
 }
 
 export const getTrendingAuthors = async ({
+  name = '',
   size = 5,
-  order = 'asc',
+  topicTag = '',
 }: TrendingOptions) => {
-  const authors = await new Promise(resolve => {
-    setTimeout(() => {
-      resolve(trendingAuthors)
-    })
+  if (__DEV__) await sleep(300)
+  const authors = await Promise.resolve(trendingAuthors.splice(0, size))
+  return authors.filter(({ subjects, firstName, lastName }) => {
+    let shouldReturn = false
+    if (name)
+      shouldReturn = `${firstName}${lastName}`.toLowerCase().includes(name)
+    if (topicTag) shouldReturn = subjects.some(({ tag }) => tag === topicTag)
+    return shouldReturn
   })
-  return authors
 }
 
-type NewReleasesOptions = {
-  authorId: string
+type GetRelatedInfoOptions = {
+  authorId?: string
   size?: number
 }
 
 export const getRelatedInfo = async ({
   size = 5,
   authorId,
-}: NewReleasesOptions) => {
+}: GetRelatedInfoOptions) => {
   const newReleases: Podcast[] = await new Promise(resolve => {
     setTimeout(() => {
       resolve(podcasts.slice(0, size))
@@ -60,4 +74,23 @@ export const getRelatedInfo = async ({
     featured,
     relatedAuthors,
   }
+}
+
+export const getAuthors = async ({
+  name = '',
+  topicTag = '',
+  size = 10,
+}: GetAuthorsOptions) => {
+  if (__DEV__) await sleep(300)
+  const authors = await Promise.resolve(trendingAuthors.splice(0, size))
+  return authors.filter(({ subjects, firstName, lastName }) => {
+    let shouldReturn = false
+    if (name)
+      shouldReturn = `${firstName}${lastName}`.toLowerCase().includes(name)
+    if (topicTag)
+      shouldReturn = subjects.some(
+        ({ tag }) => tag.toLowerCase() === topicTag.trim().toLowerCase(),
+      )
+    return shouldReturn
+  })
 }
