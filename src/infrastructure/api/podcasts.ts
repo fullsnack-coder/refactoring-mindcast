30de0250ecb8f1d348d77ee15826307122048fd1
Manuel Garcia <manueldev250799@gmail.com>
2022-01-19 23:01:26 -0500

    feat: add search authors and podcast by topics services

--- src/infrastructure/api/podcasts.ts
+++ src/infrastructure/api/podcasts.ts
@@ -1,4 +1,5 @@
 import { Episode, Podcast } from '@application/types'
+import { sleep } from '@application/utils/tools'
 import { podcastEpisodes, podcasts } from '@infrastructure/mock/apiData'
 
 //TODO: change this mock value by podcast real backend
@@ -38,24 +39,36 @@ export const getHottestPodcasts = async ({
   size = 10,
   order = 'asc',
 }: HottestOptions) => {
-  const hottestPodcasts = await new Promise(resolve => {
-    setTimeout(
-      () =>
-        resolve(
-          podcasts.sort(
-            (current, next) =>
-              current.averagePuntuation - next.averagePuntuation,
-          ),
-        ),
-      1000,
-    )
-  })
+  if (__DEV__) await sleep(100)
+  const hottestPodcasts = await Promise.resolve(
+    podcasts.sort(
+      (current, next) => current.averagePuntuation - next.averagePuntuation,
+    ),
+  )
   return hottestPodcasts
 }
 
-export const getNewReleases = async (): Promise<Podcast[]> => {
-  const newReleases: Podcast[] = await new Promise(resolve => {
-    setTimeout(() => resolve(podcasts.reverse()), 1500)
+type GetNewReleasesOptions = {
+  author?: string
+  topicTag?: string
+}
+
+export const getNewReleases = async ({
+  author,
+  topicTag,
+}: GetNewReleasesOptions): Promise<Podcast[]> => {
+  if (__DEV__) await sleep(270)
+  const newReleases: Podcast[] = await Promise.resolve(podcasts.reverse())
+  return newReleases.filter(({ subjects, author: podcastAuthor }) => {
+    let shouldReturn = false
+    if (author)
+      shouldReturn = `${podcastAuthor.firstName}${podcastAuthor.lastName}`
+        .toLowerCase()
+        .includes(author)
+    if (topicTag)
+      shouldReturn = subjects.some(
+        ({ tag }) => tag.toLowerCase() === topicTag.trim().toLowerCase(),
+      )
+    return shouldReturn
   })
-  return newReleases
 }
