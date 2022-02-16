5ad9d6147bce429793feaa743de7e5b9f7512707
Manuel Garcia <manueldev250799@gmail.com>
2022-02-16 00:29:16 -0500

    chore: add icons to notification & change main app color brightness

--- src/system/molecules/Snackbar/Snackbar.tsx
+++ src/system/molecules/Snackbar/Snackbar.tsx
@@ -1,4 +1,5 @@
 import Box, { BoxProps } from '@system/atoms/Box'
+import Separator from '@system/atoms/Separator'
 import Typography from '@system/atoms/Typography'
 
 const { Text } = Typography
@@ -8,24 +9,51 @@ type SnackbarType = 'success' | 'info' | 'warning' | 'error'
 export type Props = {
   content: string | JSX.Element
   containerProps?: BoxProps
+  left?: JSX.Element
+  right?: JSX.Element
   type: SnackbarType // TODO: make different styles for different types
 }
 
-const Snackbar: React.FC<Props> = ({ content, containerProps, type: _ }) => {
+const Snackbar: React.FC<Props> = ({
+  content,
+  left,
+  right,
+  containerProps,
+  type: _,
+}) => {
   return (
     <Box
       p={typeof content === 'string' ? 'md' : undefined}
       bg="primaryBackground"
       mx="md"
       borderRadius="sm"
+      alignItems="center"
+      flexDirection="row"
+      justifyContent="space-between"
       {...containerProps}>
+      {left ? (
+        <>
+          {left}
+          <Separator x={6} />
+        </>
+      ) : null}
       {typeof content === 'string' ? (
-        <Text color="primaryText" fontSize={16}>
+        <Text
+          color="primaryText"
+          fontSize={16}
+          textAlign="left"
+          style={{ flex: 1 }}>
           {content}
         </Text>
       ) : (
         content
       )}
+      {right ? (
+        <>
+          <Separator x={6} />
+          {right}
+        </>
+      ) : null}
     </Box>
   )
 }
